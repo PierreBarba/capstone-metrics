313a5985e28cf2948d58f7b8a5d7aa627c736c23
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-02-16 00:31:50 -0600

    app test

---
+++ src/__test__/App.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../Redux/ConfigureStore';
+import '@testing-library/jest-dom';
+
+
+test('renders learn react link', () => {
+  render(
+    <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
+  const linkElement = screen.getByText('Countries');
+  expect(linkElement).toBeInTheDocument();
+});
