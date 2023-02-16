68e90258409fa0da0325ed2bb080ea85bbd4290e
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-02-16 00:31:26 -0600

    mapList test

--- src/__test__/MapList.test.js
+++ src/__test__/MapList.test.js
@@ -7,10 +7,18 @@ import '@testing-library/jest-dom';
 import store from '../Redux/ConfigureStore';
 
 const CountriesData = [{
-  name: 'Slovakia',
-  population: 5458827,
-  flag: 'https://flagcdn.com/sk.svg',
-  id: '99999',
+  name: 'Ukraine',
+  population: 44134693,
+  flag: 'https://flagcdn.com/is.svg',
+  id: 'e115687c-660f-48e8-8da0-bf9809a9140c',
+  subregion: 'Eastern Europe',
+  coordenates: {
+    lat: 49,
+    long: 32,
+  },
+  soweek: 'monday',
+  capital: 'Kyiv',
+  coarms: 'https://mainfacts.com/media/images/coats_of_arms/ua.svg',
 }];
 
 describe('page deploy testing', () => {
